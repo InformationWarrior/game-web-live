cbfff5d52baed468ad9ff62d7d6e0a158ba79bb4
Abhishek Sharma <abhishekjps19@gmail.com>
2025-01-31 08:19:22 +0530

    RoundInfo css

--- src/Games/WheelSpin/Sections/RoundInfoSection.jsx
+++ src/Games/WheelSpin/Sections/RoundInfoSection.jsx
@@ -1,59 +1,84 @@
+// import React from "react";
+// import "../styles/RoundInfo.css";
+
+// const RoundInfoSection = () => {
+// const { gameRound, inGameMessage } = useSelector((state) => state.wheelSpin);
+
+// console.log("In game message = ", inGameMessage);
+//   return (
+//     <div className="round-info-container bg-dark text-white p-3 rounded">
+//       {/* Header Section */}
+//       <div className="row align-items-center mb-1">
+//         <div className="col">
+//           <h3 className="mb-0 fs-6 fw-medium">Round {gameRound}</h3>
+//         </div>
+//         <div className="col-auto">
+//           {/* Timer Component */}
+//           <AnotherTimerComponent />
+//         </div>
+//       </div>
+
+//       {/* Message Section */}
+//       {/* <div className="message-section bg-secondary text-white p-3 rounded">
+//         <div className="current-message-container">
+//           {inGameMessage ? (
+//             <div className="current-message py-1">{inGameMessage}</div>
+//           ) : (
+//             ""
+//           )}
+//         </div>}
+//       </div> */
+//     </div>
+//   );
+// };
+
 import React from "react";
-import "../styles/RoundInfo.css";
+import RoundInfo from "../styles/SectionModules/RoundInfo.module.css";
 import AnotherTimerComponent from "../components/AnotherTimerComponent";
 import { useSelector } from "react-redux";
+import RoundInfoContent from "../components/RoundInfoContent";
+import RoundInfoContentImage from "../components/RoundInfoContentImage";
 
-const RoundInfoSection = () => {
+function RoundInfoSection() {
   const { gameRound, inGameMessage } = useSelector((state) => state.wheelSpin);
-
   console.log("In game message = ", inGameMessage);
+
   return (
-    <div className="round-info-container bg-dark text-white p-3 rounded">
-      {/* Header Section */}
-      <div className="row align-items-center mb-1">
-        <div className="col">
-          <h3 className="mb-0 fs-6 fw-medium">Round {gameRound}</h3>
-        </div>
-        <div className="col-auto">
-          {/* Timer Component */}
-          <AnotherTimerComponent />
+    <div className={RoundInfo.content}>
+      <div className={RoundInfo.head}>
+        <p className={RoundInfo.headText}>Round {gameRound}</p>
+        <div className={RoundInfo.timerPlaceholder}>
+          <div className={RoundInfo.timer}>
+            <p className={RoundInfo.timerText}>02:00</p>
+          </div>
         </div>
       </div>
 
-      {/* Message Section */}
-      {/* <div className="message-section bg-secondary text-white p-3 rounded">
-        <div className="current-message-container">
-          {inGameMessage ? (
-            <div className="current-message py-1">{inGameMessage}</div>
-          ) : (
-            ""
-          )}
+      <div className={RoundInfo.body}>
+        <div className={RoundInfo.bodyContentGrid}>
+          <RoundInfoContentImage headingData={"0"} subHeading={"Prize Pool"} />
+          <RoundInfoContent headingData={"0/500"} subHeading={"Players"} />
+          <RoundInfoContentImage
+            headingData={"0"}
+            subHeading={"Your Entries"}
+          />
+          <RoundInfoContent headingData={"0%"} subHeading={"Your Win Chance"} />
         </div>
-      </div> */}
+        <hr className={RoundInfo.hrStyle} />
 
-      {/* Details Section */}
-      <div className="row text-center mb-3">
-        <div className="col-6">
-          <div className="fs-5 fw-bold">0.11</div>
-          <div className="text-white">Prize Pool</div>
-        </div>
-        <div className="col-6">
-          <div className="fs-5 fw-bold">2/500</div>
-          <div className="text-white">Players</div>
-        </div>
-        <div className="col-6">
-          <div className="fs-5 fw-bold">0</div>
-          <div className="text-white">Your Entries</div>
-        </div>
-        <div className="col-6">
-          <div className="fs-5 fw-bold">0%</div>
-          <div className="fs-6 text-white">Your Win Chance</div>
+        <div className={RoundInfo.bodyFooter}>
+          <RoundInfoContent
+            headingData={"-----"}
+            subHeading={"Your Future Entries"}
+          />
+          <RoundInfoContentImage
+            headingData={"-----"}
+            subHeading={"Total (0 Avg)"}
+          />
         </div>
       </div>
-
-      <hr className="divider my-3" />
     </div>
   );
-};
+}
 
 export default RoundInfoSection;
